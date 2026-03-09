34610d7f2e0bbd66a373b64a1d877a5388610eee
unknown <shikha4633@gmail.com>
2026-03-09 12:18:15 +0530

    change updated

--- src/components/auth/AuthListener.jsx
+++ src/components/auth/AuthListener.jsx
@@ -1,15 +1,22 @@
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../../firebase";
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AuthListener = () => {
+  const navigate = useNavigate();
+
   useEffect(() => {
     const unsub = onAuthStateChanged(auth, (user) => {
       console.log("Auth state changed:", user);
+
+      if (user) {
+        navigate("/home");
+      }
     });
 
     return () => unsub();
-  }, []);
+  }, [navigate]);
 
   return null;
 };
